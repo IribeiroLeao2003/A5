fed2343256b535d557a8dd830368c6e003087250
IribeiroLeao2003 <96347353+IribeiroLeao2003@users.noreply.github.com>
2022-12-06 14:15:31 -0500

    Infinite loop working

---
+++ il_asm.s
@@ -0,0 +1,479 @@
+
+
+.code 16                    @ This directive selects the instruction set being generated.
+                            @ The value 16 selects Thumb, with the value 32 selecting ARM.
+.text                       @ Tell the assembler that the upcoming section is to be considered
+                            @ assembly language instructions - Code section (text -> ROM) 
+
+@@ Function Header Block 
+
+.align 2                    @ Code alignment - 2^n alignment (n=2)
+                            @ This causes the assembler to use 4 byte alignment
+.syntax unified             @ Sets the instruction set to the new unified ARM + THUMB
+                            @ instructions. The default is divided (separate instruction sets)
+.global add_test            @ Make the symbol name for the function visible to the linker
+.code 16                    @ 16bit THUMB code (BOTH .code and .thumb_func are required)
+.thumb_func                 @ Specifies that the following symbol is the name of a THUMB 
+
+.type add_test, %function   @ Declares that the symbol is a function (not strictly required)  
+
+
+
+
+
+.global ilGame              @ Make the symbol name for the function visible to the linker
+.code 16                    @ 16bit THUMB code (BOTH .code and .thumb_func are required)
+.thumb_func  
+                            @ encoded function. Necessary for interlinking between ARM and THUMB code.
+
+.type ilGame, %function     @ Declares that the symbol is a function (not strictly required) 
+ 
+
+.align 2                    @ Code alignment - 2^n alignment (n=2)
+                            @ This causes the assembler to use 4 byte alignment
+.syntax unified             @ Sets the instruction set to the new unified ARM + THUMB
+.global ilTilt              @ Make the symbol name for the function visible to the linker
+.code 16                    @ 16bit THUMB code (BOTH .code and .thumb_func are required)
+.thumb_func  
+                            @ encoded function. Necessary for interlinking between ARM and THUMB code.
+
+.type ilTilt, %function     @ Declares that the symbol is a function (not strictly required) 
+
+
+
+.global il_led_demo_a2      @ Make the symbol name for the function visible to the linker
+.code 16                    @ 16bit THUMB code (BOTH .code and .thumb_func are required)
+.thumb_func  
+                            @ encoded function. Necessary for interlinking between ARM and THUMB code.
+
+.type il_led_demo_a2, %function @ Declares that the symbol is a function (not strictly required) 
+ 
+
+
+
+
+.align 2                    @ Code alignment - 2^n alignment (n=2)
+                            @ This causes the assembler to use 4 byte alignment
+.syntax unified             @ Sets the instruction set to the new unified ARM + THUMB
+                            @ instructions. The default is divided (separate instruction sets)
+.global _il_lab_tick      @ Make the symbol name for the function visible to the linker
+.code 16                    @ 16bit THUMB code (BOTH .code and .thumb_func are required)
+.thumb_func  
+                            @ encoded function. Necessary for interlinking between ARM and THUMB code.
+
+.type _il_lab_tick, %function @ Declares that the symbol is a function (not strictly required)  
+
+
+
+.align 2                    @ Code alignment - 2^n alignment (n=2)
+                            @ This causes the assembler to use 4 byte alignment
+.syntax unified             @ Sets the instruction set to the new unified ARM + THUMB
+                            @ instructions. The default is divided (separate instruction sets)
+.global ilWatchD      @ Make the symbol name for the function visible to the linker
+.code 16                    @ 16bit THUMB code (BOTH .code and .thumb_func are required)
+.thumb_func  
+                            @ encoded function. Necessary for interlinking between ARM and THUMB code.
+
+.type ilWatchD, %function @ Declares that the symbol is a function (not strictly required) 
+
+
+
+
+
+
+
+    .code 16                    @ 16bit THUMB code (BOTH .code and .thumb_func are required)
+    .text 
+                            @ encoded function. Necessary for interlinking between ARM and THUMB code.
+.global ticks_test      @ Make the symbol name for the function visible to the linker
+.type ticks_test, %function @ Declares that the symbol is a function (not strictly required)
+
+
+ 
+
+
+
+
+
+
+ilWatchD:  
+    push {r4-r9, lr} 
+    mov   r4, r0    @ timeout 
+    mov   r5, r1    @ delay 
+    mov   r7, #0    @ LED start 
+    mov   r6, #8    @ LED finish 
+    mov   r8, #0    @ For infinite loop
+    mov   r9, #0
+
+    loop1: 
+    mov r0, r7          @ blink light 
+    bl   BSP_LED_Toggle  
+
+    mov  r0, r5         @ activate delay
+    bl   busy_delay 
+
+    mov r0, r7          @ turn off light
+    bl   BSP_LED_Toggle  
+
+    mov  r0, r5         @ activate delat 
+    bl   busy_delay
+    
+    add r7, r7, #1      @ add 1 to move to next LED 
+    sub r6, r6, #1      @ take 1 from total LED
+                    
+    cmp r6, #0          @ compare to see if it reached the ed 
+    bgt loop1  
+ 
+     
+    mov r7, #0 
+    mov r6, #0 
+
+
+    
+
+    pop  {r4-r9, lr}    
+
+    bx lr   
+
+
+
+
+@ Function Declaration : int il_led_demo_a2(int count, int delay)
+@
+@ Input: r0, r1 (i.e. r0 holds count, r1 holds delay)
+@
+il_led_demo_a2:   
+    push {r4-r7, lr} 
+    mov r4, r0      @ count 
+    mov r5, r1      @ delay 
+    mov r6, #8      @ full loop 
+    mov r7, #0      @ led 
+
+@ Function Declaration : loop 
+@
+@ Description: Works as loop and fixes delay 
+@
+loop:  
+    mov r0, r7 
+    bl   BSP_LED_Toggle  
+
+    mov  r0, r5
+    bl   busy_delay 
+
+    mov r0, r7 
+    bl   BSP_LED_Toggle  
+
+    mov  r0, r5
+    bl   busy_delay
+    
+    add r7, r7, #1 
+    sub r6, r6, #1
+                    
+    cmp r6, #0 
+    bgt loop  
+ 
+    sub r4, r4, #1 
+    mov r7, #0 
+    mov r6, #0 
+
+
+    cmp r4, r0  
+    bgt loop 
+
+    pop  {r4-r7, lr}    
+
+    bx lr 
+  
+    
+    
+
+@ Function Declaration : ilGame 
+@
+@ Parameters: count, delay and pattern 
+@
+ 
+
+
+ ilGame: 
+    push {r4-r10, lr}   @ Saving register values for delay, pattern, target.
+    mov r4, r0          @ Move our delay value to r4 
+    mov r5, r1          @ Move our pattern value to r5 
+    mov r6, r2          @ Move our target value to r6   
+    mov r7, #0x30
+
+
+
+ledlight_loop:  
+
+    ldrb r8, [r5]            @ load the pattern on r8
+    sub  r9, r8, r7          @ converting the number from ASCII to decimal   
+
+    
+    mov  r0, r9              @ Moving r9 back to r0 for pattern   
+    bl   BSP_LED_Toggle   
+
+    mov  r0, r4              @ Moving delay back to r0 
+    bl   busy_delay  
+
+
+    
+    mov  r0, r9              @ Moving r9 back to r0 for pattern
+    bl   BSP_LED_Toggle  
+
+    mov  r0, r4              @ Moving delay back to r0 
+    bl   busy_delay  
+
+
+    mov  r0, r9              @ Moving r9 back to r0 for pattern
+    bl   BSP_LED_Toggle  
+
+    mov  r0, r4              @ Moving delay back to r0 
+    bl   busy_delay  
+    
+
+    mov  r0, r9              @ Moving r9 back to r0 for pattern
+    bl   BSP_LED_Toggle  
+
+    mov  r0, r4              @ Moving delay back to r0 
+    bl   busy_delay   
+
+    
+    
+    
+        
+
+    cmp r8, #0               @ Compare to see if string is empty  
+    add r5, r5, #1           @ increase pattern by 1 to move to the next light   
+    bgt ledlight_loop        @ If no then continue  
+
+    cmp r10, #5              @ reset the values for infiniue
+    mov r9, r5 
+    mov r8, #0
+    bne ledlight_loop
+
+    
+
+    pop  {r4-r10, lr}     
+    bx lr  
+    
+
+a4_ticks: .word 0 
+a4_ticks_to_blink: .word 0 
+
+ticks_test: 
+
+    ldr r1, =a4_ticks 
+    str r0, [r1]  
+    bx lr 
+_il_lab_tick: 
+    push {lr} 
+
+    
+    ldr r1, =a4_ticks 
+    ldr r0, [r1] 
+    subs r0, r0, #1 
+
+    ble do_nothing 
+
+    str r0, [r1]
+
+    ldr r1, =a4_ticks_to_blink
+    ldr r0, [r1] 
+    subs r0,r0, #1  
+
+    str r0, [r1] 
+
+    bgt do_nothing   
+
+    mov r0, #0 
+    bl BSP_LED_Toggle 
+
+    ldr r1, =a4_ticks_to_blink 
+    mov r0, #500 
+    str r0,[r1]
+
+     
+    do_nothing:  
+
+    pop {lr} 
+    bx lr  
+
+    .size _il_lab_tick, .-_il_lab_tick 
+
+
+
+
+
+.equ Y_HI, 0x2B 
+.equ X_HI, 0x29  
+.equ X_LO, 0x28 
+.equ I2C_Address, 0x32 
+
+ilTilt: 
+    push {lr}  
+ 
+    mov r4, r0  
+
+    mov r0, r4
+    bl busy_delay 
+    
+
+    mov r0, #I2C_Address 
+    mov r1, #X_HI
+    bl COMPASSACCELERO_IO_Read
+     
+    sxtb r0, r0  
+    mov r5, r0 @X
+
+
+    mov r0, #I2C_Address  
+    mov r1, #Y_HI 
+    bl COMPASSACCELERO_IO_Read
+
+    sxtb r0, r0  
+    mov r6, r0  @Y
+
+    
+
+    pop {lr}
+    bx lr   
+
+    .size ilTilt, .-ilTilt  
+ 
+
+
+
+ x_y_toled:  
+    push {lr}
+    mov r4, r0  @ X
+    mov r5, r1  @ Y  
+ 
+    
+    cmp r5, #-64  
+    bgt led_7  
+    cmp r4, #0
+    beq led_7
+
+
+    cmp r4, #32
+    bgt led_6 
+    cmp r5, #-32  
+    bgt led_6  
+    pop {lr} 
+    bx lr       
+
+
+
+    cmp r4, #-32
+    bgt led_5 
+    cmp r5, #-32  
+    bgt led_5 
+    pop {lr} 
+    bx lr 
+
+    cmp r5, #0  
+    beq led_4  
+    cmp r4, #64 
+    bgt led_4 
+
+    cmp r5, #0  
+    beq led_3  
+    cmp r4, #-64
+    bgt led_3 
+
+
+    cmp r4, #32
+    bgt led_2 
+    cmp r5, #32  
+    bgt led_2   
+
+    cmp r4, #-32
+    blt led_1
+    cmp r5, #32  
+    bgt led_1  
+
+    
+    cmp r5, #64  
+    bgt led_0  
+    cmp r4, #0
+    beq led_0 
+
+
+
+    
+
+    
+
+
+led_7: 
+    mov r0, #7 
+    bx lr 
+
+led_6: 
+    mov r0, #6 
+    bx lr 
+
+led_5:
+    mov r0, #5 
+    bx lr 
+
+led_4: 
+    mov r0, #4 
+    bx lr 
+
+led_3: 
+    mov r0, #3 
+    bx lr  
+
+led_2: 
+    mov r0, #2  
+    bx lr 
+
+led_1: 
+    mov r0, #1 
+    bx lr 
+
+led_0: 
+    mov r0, #0
+    bx lr
+
+
+
+
+@ Function Declaration : int add_test(int x, int y)
+@
+@ Input: r0, r1 (i.e. r0 holds x, r1 holds y)
+@ Returns: r0
+@
+@ Here is the actual add_test function 
+add_test: 
+    push {lr}                @ Put aside registers we want to restore later
+
+    mov  r0, #1              @ r0 holds our argument for the LED toggle function
+                             @ So pass it a value
+
+    bl   BSP_LED_Toggle      @ call BSP C function using Branch with Link (bl) 
+    ldr  r1, =myTickCount
+    ldr  r0, [r1]
+    
+    pop  {lr}                @ Bring all the register values back
+
+    bx lr    
+
+    add r0, r0, r1
+    bx lr 
+    .size add_test, .-add_test  
+
+busy_delay:
+    push {r5}
+    mov r5, r0 
+
+delay_1oop:
+    subs r5, r5, #1
+    bge delay_1oop
+    mov r0, #0                @ Return zero (success)
+    pop {r5}
+    bx lr                     @ Return (Branch eXchange) to the address in the link register (lr) 
+
+.size  il_led_demo_a2, .-il_led_demo_a2 
+.end
---
+++ il_hook.c
@@ -0,0 +1,280 @@
+/*
+* C to assembler menu hook
+*
+*/
+#include <stdio.h>
+#include <stdint.h>
+#include <ctype.h>
+#include "common.h" 
+#include "stm32f3xx_hal.h"
+#include "stm32f3_discovery.h"
+#include "stm32f3_discovery_accelerometer.h"
+#include "stm32f3_discovery_gyroscope.h"   
+#include "watchdog.h"
+ 
+
+
+int add_test(int x, int y, int delay); 
+int il_led_demo_a2(int count, int delay);    
+int ilGame(int delay, char *pattern, int target); 
+int ticks_test(int time);  
+void _il_lab_tick(void);   
+int ilTilt(int choice);  
+int ilWatchD(int timeout, int delay); 
+
+
+/*
+* Function Name: _il_A2 
+* Paremeters: Action  
+* Description: Collects user input and sends to assembly code to verify and do loop 
+*/
+void _il_A2(int action)
+{  
+//variables  created
+    uint32_t count;
+    int fetch_status; 
+    uint32_t delay;
+    
+    //fecthes user input for count 
+    fetch_status = fetch_uint32_arg(&count);
+
+    if(fetch_status) {
+    // Use a default value
+    count = 1;
+    } 
+
+    //fetches a user input for delay 
+    fetch_status = fetch_uint32_arg(&delay);
+
+    if(fetch_status) {
+    // Use a default value
+    delay = 0xFFFFFF;
+    }
+    if(action==CMD_SHORT_HELP) return;
+    if(action==CMD_LONG_HELP) {
+    printf("Addition Test\n\n"
+    "This command tests new addition function\n"
+    );
+    return;
+    }
+        printf("add_test returned: %d\n", il_led_demo_a2(count, delay) );
+    }
+ADD_CMD("add2", _il_A2,"Test the new add2 function");
+
+  
+
+
+
+
+
+
+
+ void gameOn(int action)
+{   
+
+//variables  created 
+uint32_t target;
+int fetch_status; 
+uint32_t delay; 
+char *pattern; 
+
+
+
+//fecthes user input for delay 
+fetch_status = fetch_uint32_arg(&delay);
+
+if(fetch_status) {
+// Use a default value
+delay = 0xFFFFFF;
+} 
+
+//fetches a user input for pattern 
+fetch_status = fetch_string_arg(&pattern);
+
+if(fetch_status) {
+// Use a default value 
+pattern = "012345678";
+} 
+
+//fecthes user input for target 
+fetch_status = fetch_uint32_arg(&target);
+
+if(fetch_status) {
+// Use a default value
+target = 1; 
+} 
+
+if(action==CMD_SHORT_HELP) return;
+if(action==CMD_LONG_HELP) {
+printf("Game \n\n"
+"This command starts a game of blinking lights \n"
+);
+return;
+}    
+
+
+
+if(ilGame(delay, pattern, target) == 1){ 
+    printf("ilGame returned: You Won"); 
+    } else { 
+    printf("ilGame returned: You Lost"); 
+    } 
+
+}
+ADD_CMD("ilGame", gameOn,"Test the new Game function") 
+ 
+
+
+
+
+void AddTest(int action)
+{ 
+uint32_t delay;
+int fetch_status; 
+
+fetch_status = fetch_uint32_arg(&delay); 
+
+if(fetch_status) {
+// Use a default delay value
+delay = 0xFFFFFF;
+}
+// When we call our function, pass the delay value.
+// printf(“<<< here is where we call add_test – can you add a third parameter? >>>”); 
+
+if(action==CMD_SHORT_HELP) return;
+if(action==CMD_LONG_HELP) {
+printf("Addition Test\n\n"
+"This command tests new addition function\n"
+);
+return;
+}
+printf("add_test returned: %d\n", add_test(99, 87, delay) );
+}
+ADD_CMD("add", AddTest,"Test the new add function")  
+
+
+
+void TicksTest(int action)
+{  
+if(action==CMD_SHORT_HELP) return;
+if(action==CMD_LONG_HELP) {
+printf("Tick Test\n\n"
+"This command tests new tick test \n"
+);
+return;
+} 
+
+
+uint32_t user_input; 
+int fetch_status; 
+
+fetch_status = fetch_uint32_arg(&user_input); 
+
+if(fetch_status) {
+
+    user_input = 5000;
+}
+
+
+printf("ticks_test returned: %d\n", ticks_test(user_input) );  
+
+
+
+}
+ADD_CMD("ticktest",TicksTest,"Test the new TicksTest function")  
+ 
+
+
+
+
+
+void ilTiltGame(int action)
+{  
+if(action==CMD_SHORT_HELP) return;
+if(action==CMD_LONG_HELP) {
+printf("ilTilt\n\n"
+"This command tests new tilt game \n"
+);
+return;
+} 
+
+
+uint32_t user_input; 
+int fetch_status; 
+
+fetch_status = fetch_uint32_arg(&user_input); 
+
+if(fetch_status) {
+
+    user_input = 0xFFFFF;
+}
+
+
+for (int i = 0; i < 100; i++){ 
+    printf("ilTilt returned: %d\n", ilTilt(user_input) );  
+}
+
+
+
+
+}
+ADD_CMD("ilTilt",ilTiltGame,"Test the new TicksTest function")    
+
+
+void testWatchdog(int action)
+{  
+    mes_InitIWDG();   
+    mes_IWDGStart(); 
+    mes_IWDGRefresh(); 
+
+}
+ADD_CMD("watchDogs",testWatchdog,"loool")   
+ 
+
+
+ void _ilWatch(int action)
+{  
+if(action==CMD_SHORT_HELP) return;
+if(action==CMD_LONG_HELP) {
+printf("ilWatch\n\n"
+"The command tests the new watchdog game \n"
+);
+return;
+} 
+
+
+uint32_t timeout;  
+uint32_t delay;   
+
+int fetch_status; 
+
+fetch_status = fetch_uint32_arg(&delay); 
+
+if(fetch_status) {
+
+    delay = 0xFFFFF;
+} 
+
+fetch_status = fetch_uint32_arg(&timeout); 
+
+if(fetch_status) {
+
+    timeout = 0xFFFFF;
+}
+
+
+while(1){ 
+    printf("ilWatchD returned: %d\n", ilWatchD(timeout, delay) ); 
+}
+
+
+
+
+}
+ADD_CMD("ilWatch",_ilWatch,"Test the new _ilWatch function")  
+
+
+
+
+
+
